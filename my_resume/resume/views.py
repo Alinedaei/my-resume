a494d91157229e8ce32d6f788321a37b8cd09c67
alinedaei <alinedaeitoosi@gmail.com>
2024-08-26 14:45:17 +0330

    dashbord4

--- my_resume/resume/views.py
+++ my_resume/resume/views.py
@@ -1,29 +1,31 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from .forms import LoginForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
 
 def login_view(request):
     if request.method == 'POST':
-        form = LoginForm(request, data=request.POST)
+        form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
-            user = authenticate(request, username=username, password=password)
+            user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                return redirect('dashboard')  # هدایت کاربر به داشبورد بعد از لاگین
+                return redirect('dashboard')  # هدایت به داشبورد
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
     else:
-        form = LoginForm()
+        form = AuthenticationForm()
     return render(request, 'login.html', {'form': form})
-
-@login_required
-def resume_view(request):
-    return render(request, 'resume.html')
 
 @login_required
 def dashboard_view(request):
     return render(request, 'dashboard.html')
 
-def index_view(request):
-    return render(request, 'index.html')
+@login_required
+def resume_view(request):
+    return render(request, 'resume.html')
